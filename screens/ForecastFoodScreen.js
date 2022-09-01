be65fbdbb313500bcd1c3505d204d2c7e452dbcc
rae <jkraezou@gmail.com>
2022-09-02 11:08:33 +1200

    forecast screens

--- screens/ForecastFoodScreen.js
+++ screens/ForecastFoodScreen.js
@@ -6,9 +6,9 @@ export default function ForecastFoodScreen({ navigation }) {
         <View style={styles.container}>
             <Text
                 style={styles.paragraph}>
-                    Based on Mobile AppSec Verification Standard: 
-                    Data Storage and Privacy Requirements, 
-                    we will not save your data on any server and therefore that no one but you can access it. 
+                    No sensitive data is written to application logs.
+                    No sensitive data is shared with third parties unless it is a necessary part of the architecture. 
+                    
                 </Text>
         </View>
     );
@@ -23,9 +23,13 @@ const styles =  StyleSheet.create({
     },
     paragraph: {
       marginTop: 5,
-      paddingVertical: 10,
       fontSize: 15,
       color: "#595D58",
       justifyContent: "space-between",
     },
+    paragraph2: {
+        fontSize: 15,
+        color: "#595D58",
+        justifyContent: "space-between",
+      },
   });
--- screens/ForecastTransScreen.js
+++ screens/ForecastTransScreen.js
@@ -6,9 +6,8 @@ export default function ForecastTransScreen({ navigation }) {
         <View style={styles.container}>
             <Text
                 style={styles.paragraph}>
-                    Based on Mobile AppSec Verification Standard: 
-                    Data Storage and Privacy Requirements, 
-                    we will not save your data on any server and therefore that no one but you can access it. 
+                    No sensitive data is written to application logs.
+                    No sensitive data is shared with third parties unless it is a necessary part of the architecture. 
                 </Text>
         </View>
     );
